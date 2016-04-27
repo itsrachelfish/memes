bdd68c60b0cd1a45de0ff8a7cba6d5905db7ec3e
Rachel Fish <itsrachelfish@gmail.com>
2016-04-26 23:24:23 -0600

    New button to add images to the page

--- src/js/main.js
+++ src/js/main.js
@@ -7,4 +7,10 @@ $(document).ready(function()
     {
         var webcam = new Webcam('.webcam');
     });
+
+    $('.add-image').on('click', function()
+    {
+        var src = prompt("Enter an image URL");
+        $('.workspace').append('<img src="'+src+'">');
+    });
 });
