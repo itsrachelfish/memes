c3835dd7eed23c728f6bd251609543d5f02e57d4
Rachel Fish <itsrachelfish@gmail.com>
2016-08-24 06:46:58 -0600

    Misc updates, renamed project on github

--- src/js/ui/hover.js
+++ src/js/ui/hover.js
@@ -103,7 +103,7 @@ var hover =
         // Make sure no other menus are still active
         hover.stop();
 
-        $(template).style({'height': size.height + 'px', 'width': size.width + 'px', 'z-index': helper.layers + 1});
+        $(template).style({'height': size.height + 'px', 'width': size.width + 'px', 'z-index': zindex});
         $(template).transform(element.transform);
 
         $(template).on('click', function(event)
