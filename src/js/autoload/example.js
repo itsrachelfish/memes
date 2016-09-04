22252d0d8122240cba4d1d2fb4ff7713e83f2f9e
Rachel Fish <itsrachelfish@gmail.com>
2016-09-04 08:01:07 -0600

    Started setting up autoload stuff

---
+++ src/js/autoload/example.js
@@ -0,0 +1,87 @@
+module.exports = {
+    "title": "Example project",
+    "author": "",
+    "camera": false,
+    "frame": 0,
+    "frames": [
+        {
+            "background": {
+                "background": "linear-gradient(45deg, #f06, yellow)",
+                "picker": "#000000",
+                "image": "",
+                "repeat": "repeat",
+                "position": ""
+            },
+            "s4o2g3imk9": {
+                "position": {
+                    "left": 446,
+                    "top": 96
+                },
+                "transform": {
+                    "translate": [
+                        "446px",
+                        "96px"
+                    ]
+                },
+                "layer": "2"
+            },
+            "byqcz5ap2j": {
+                "position": {
+                    "left": 358,
+                    "top": 9
+                },
+                "transform": {
+                    "translate": [
+                        "358px",
+                        "9px"
+                    ]
+                },
+                "layer": "6"
+            }
+        }
+    ],
+    "objects": {
+        "s4o2g3imk9": {
+            "type": "text",
+            "centered": true,
+            "text": "Hello there",
+            "size": "34",
+            "color": "white",
+            "image": "",
+            "border": {
+                "enabled": true,
+                "size": "10",
+                "color": "black",
+                "image": "img/rainbow-spiral.gif"
+            },
+            "position": {
+                "left": 446,
+                "top": 96
+            },
+            "transform": {
+                "translate": [
+                    "446px",
+                    "96px"
+                ]
+            },
+            "layer": "2"
+        },
+        "byqcz5ap2j": {
+            "type": "image",
+            "url": "img/sparkles.gif",
+            "position": {
+                "left": 427,
+                "top": 82
+            },
+            "transform": {
+                "translate": [
+                    "427px",
+                    "82px"
+                ]
+            },
+            "layer": "5"
+        }
+    },
+    "created": "2016-09-04T13:56:36.260Z",
+    "modified": "2016-09-04T13:58:50.918Z"
+};
---
+++ src/js/config.example.js
@@ -0,0 +1,10 @@
+var config =
+{
+    autoload:
+    {
+        enabled: true,
+        file: require('./autoload/example.js')
+    }
+};
+
+module.exports = config;
